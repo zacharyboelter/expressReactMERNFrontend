bdeecd22571b40a8f05611ef441ec49d5c41c2c8
Zachary Boelter <zboelter@gmail.com>
2021-10-19 14:34:03 -0500

    frontend final

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// IMPORT SCSS FILE TO BE SOURCE OF STYLING
 import './index.css';
+// IMPORT ROUTER
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
